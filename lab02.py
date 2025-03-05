775e889d4baa913457b97f4f4598cc8e26bfc8ea
csintalanmate <125445719+csintalanmate@users.noreply.github.com>
2025-03-05 17:08:42 +0100

    Add files via upload

--- lab02.py
+++ lab02.py
@@ -17,6 +17,42 @@ class kancso_prolema:
         gyerekek = []
 
         if a1 != 0 and a2 != self.MAX2:
+            T = min(a1, self.MAX2 - a2)
+            uj_allapot = (a1 - T, a2 + T, a3)
+            gyerekek.append(("1->2", uj_allapot))
+
+
+        if a1 != 0 and a3 != self.MAX3:
+            T = min(a1, self.MAX3 - a3)
+            uj_allapot = (a1 - T, a2, a3 + T)
+            gyerekek.append(("1->3", uj_allapot))
+
+
+        if a2 != 0 and a1 != self.MAX1:
+            T = min(a2, self.MAX1 - a1)
+            uj_allapot = (a1 + T, a2 - T, a3)
+            gyerekek.append(("2->1", uj_allapot))
+
+
+        if a2 != 0 and a3 != self.MAX3:
+            T = min(a2, self.MAX3 - a3)
+            uj_allapot = (a1, a2 - T, a3 + T)
+            gyerekek.append(("2->3", uj_allapot))
+
+
+        if a3 != 0 and a1 != self.MAX1:
+            T = min(a3, self.MAX1 - a1)
+            uj_allapot = (a1 + T, a2, a3 - T)
+            gyerekek.append(("3->1", uj_allapot))
+
+
+        if a3 != 0 and a2 != self.MAX2:
+            T = min(a3, self.MAX2 - a2)
+            uj_allapot = (a1, a2 + T, a3 - T)
+            gyerekek.append(("3->2", uj_allapot))
+
+
+        return gyerekek
 
 
 
